4626ac92cf387908255afce3c654ac061d0f0fd2
David <d.gamon.p@gmail.com>
2020-02-19 09:52:17 -0400

    title ah

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Mi aplicacion'),
+      home: MyHomePage(title: 'Mi aplicacion 2'),
     );
   }
 }
